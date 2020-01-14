e90b630b576a193f639d781f37fb6a6ff0b8c3c5
unknown <storm_iz_here@yahoo.com>
2020-01-15 00:47:04 +0800

    URLs and Views for static pages. No content yet.

--- cos_site/urls.py
+++ cos_site/urls.py
@@ -5,4 +5,10 @@ from cos_site import views
 
 urlpatterns = [
     url(r'^$', views.index, name='site_index'),
+    url(r'^dashboard_profile/$', views.dashboard_profile, name='dashboard_profile'),
+    url(r'^focal/$', views.focal, name='focal'),
+    url(r'^plateiq/$', views.plateiq, name='plateiq'),
+    url(r'^persistiq/$', views.persistiq, name='persistiq'),
+    url(r'^smartalto/$', views.smart_alto, name='smart_alto'),
+    url(r'^knowledgebase/$', views.knowledge_base, name='knowledge_base'),
 ]
--- cos_site/views.py
+++ cos_site/views.py
@@ -3,5 +3,28 @@ from django.shortcuts import render
 
 
 def index(request):
-
     return render(request, 'cos_site/index.html', {})
+
+
+def dashboard_profile(request):
+    return render(request, 'cos_site/dashboard_profile.html', {})
+
+
+def focal(request):
+    return render(request, 'cos_site/focal.html', {})
+
+
+def plateiq(request):
+    return render(request, 'cos_site/plateiq.html', {})
+
+
+def persistiq(request):
+    return render(request, 'cos_site/persistiq.html', {})
+
+
+def smart_alto(request):
+    return render(request, 'cos_site/smart_alto.html', {})
+
+
+def knowledge_base(request):
+    return render(request, 'cos_site/knowledge_base.html', {})
