65a036394f758bc89c290bbe73e0b74d38fa19a8
Jeanne Kevin T. Decena <storm_iz_here@yahoo.com>
2021-10-05 18:03:27 +0800

    Added auth

--- cos_site/views.py
+++ cos_site/views.py
@@ -1,9 +1,47 @@
 # Core
-from django.shortcuts import render
+from django.contrib import auth, messages
+from django.shortcuts import redirect, render
 
 
 def index(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            # If correct username and password, login the user
+            auth.login(request, user)
+            return redirect('dashboard_profile')
+        else:
+            # Neither username nor password is correct.
+            messages.error(request, ' Unknown email address. Check again or try your username.')
+            return redirect('login')
+
     return render(request, 'cos_site/index.html', {})
+
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            # If correct username and password, login the user
+            auth.login(request, user)
+            return redirect('dashboard_profile')
+        else:
+            # Neither username nor password is correct.
+            messages.error(request, ' Unknown email address. Check again or try your username.')
+            return redirect('login')
+
+    return render(request, 'cos_site/login.html', {})
+
+
+def logout(request):
+    auth.logout(request)
+    return render(request, 'cos_site/logout.html')
 
 
 def dashboard_profile(request):
